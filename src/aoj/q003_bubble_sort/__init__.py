0d64ae7ba5bc7907addb52a2b966e91c42266ea2
羽毛笔 <quills@192.168.2.9>
2025-06-28 15:42:19 +0800

    fix: bubble sort

--- src/aoj/q003_bubble_sort/__init__.py
+++ src/aoj/q003_bubble_sort/__init__.py
@@ -3,12 +3,14 @@ def main():
     arr = list(map(int, input().split(' ')))
     flag = True
     count = 0
+    pos = 0
     while flag:
         flag = False
-        for i in range(n - 1, 0, -1):
+        for i in range(n - 1, pos, -1):
             if arr[i] < arr[i - 1]:
                 arr[i], arr[i - 1] = arr[i - 1], arr[i]
                 flag = True
                 count += 1
+        pos += 1
     print(' '.join(map(str, arr)))
     print(count)
