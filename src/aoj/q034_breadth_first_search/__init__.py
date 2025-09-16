fd5f41ceb76d27c4c991895d08feb8be2fe48fd9
羽毛笔 <quills@192.168.1.5>
2025-09-16 11:31:33 +0800

    feat: graph dfs queue implement

--- src/aoj/q034_breadth_first_search/__init__.py
+++ src/aoj/q034_breadth_first_search/__init__.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 def recursive_solution():
     n = int(input())
     graph = [[0 for _ in range(n)] for _ in range(n)]
@@ -24,12 +27,31 @@ def recursive_solution():
 
 def queue_solution():
     n = int(input())
-    graph = [[0 for _ in range(n)] for _ in range(n)]
+    graph: list[list[int]] = [[] for _ in range(n)]
     for i in range(n):
         _, deg, *edges = map(int, input().split(" "))
         for j in range(deg):
-            graph[i][edges[j] - 1] = 1
+            graph[i].append(edges[j] - 1)
+
+    depth_arr = [-1] * n
+    visit_arr = [False] * n
+
+    def bfs(v: int):
+        queue = deque([v])
+        depth_arr[v] = 0
+        visit_arr[v] = True
+        while len(queue) > 0:
+            cur_v = queue.popleft()
+            for next_v in graph[cur_v]:
+                if not visit_arr[next_v]:
+                    visit_arr[next_v] = True
+                    depth_arr[next_v] = depth_arr[cur_v] + 1
+                    queue.append(next_v)
+
+    bfs(0)
+    for i in range(n):
+        print(f"{i + 1} {depth_arr[i]}")
 
 
 def main():
-    recursive_solution()
+    queue_solution()
