8ca5d04d6e00419f5ad98d56c7339a6ebf158901
羽毛笔 <quills@192.168.1.5>
2025-09-16 09:53:53 +0800

    fix: binary search

--- src/aoj/q012_binary_search/__init__.py
+++ src/aoj/q012_binary_search/__init__.py
@@ -2,20 +2,16 @@ from math import floor
 
 
 def binary_search(arr: list[int], n: int, target: int):
-    if int(target) < int(arr[0]):
-        return False
-    if int(target) > int(arr[n - 1]):
-        return False
     left = 0
     right = n
-    while left <= right:
+    while left < right:
         mid = floor((left + right) / 2)
         if arr[mid] == target:
             return True
         if arr[mid] < target:
             left = mid + 1
         else:
-            right = mid - 1
+            right = mid
     return False
 
 
